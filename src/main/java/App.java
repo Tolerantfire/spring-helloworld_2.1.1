712c15813b210910255c124f990878a7f6923828
tolerantfire <andrewmkrtchan91@gmail.org>
2020-10-21 23:13:37 +0300

    v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,10 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
+        Cat catBean = applicationContext.getBean(Cat.class);
+        Cat catBean2 = applicationContext.getBean(Cat.class);
+        System.out.println(bean.equals(bean2));
+        System.out.println(catBean.equals(bean2));
     }
 }
